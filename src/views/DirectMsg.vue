45b3de1d5ec7b99312318758897225a0dca2f1a4
sangpok <y_y5760y_y@naver.com>
2022-05-02 16:23:58 +0900

    test

--- src/views/DirectMsg.vue
+++ src/views/DirectMsg.vue
@@ -1,5 +1,40 @@
 <template>
-    <div>DirectMsg Page</div>
+    <section class="container">
+        <div class="item">
+            <div class="img-wrapper">
+                <img :src="require('@/assets/1.jpg')" alt="" />
+            </div>
+            <div class="elegroup">
+                <span class="info1">이름</span>
+                <span class="info2">소개글</span>
+            </div>
+            <button class="btn">버튼</button>
+        </div>
+        <div class="item">
+            <img :src="require('@/assets/2.jpg')" alt="" />
+            <div class="elegroup">
+                <span class="info1">이름</span>
+                <span class="info2">소개글</span>
+            </div>
+            <button class="btn">버튼</button>
+        </div>
+        <div class="item">
+            <img :src="require('@/assets/3.jpg')" alt="" />
+            <div class="elegroup">
+                <span class="info1">이름</span>
+                <span class="info2">소개글</span>
+            </div>
+            <button class="btn">버튼</button>
+        </div>
+        <div class="item">
+            <img :src="require('@/assets/5.jpg')" alt="" />
+            <div class="elegroup">
+                <span class="info1">이름</span>
+                <span class="info2">소개글</span>
+            </div>
+            <button class="btn">버튼</button>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -18,3 +53,7 @@
         methods: {},
     };
 </script>
+
+<style lang="scss" scoped>
+    @import '@/scss/DirectMsg';
+</style>
--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -1,80 +1,141 @@
 <template>
-    <section id="feed-container">
-        <section id="story-field">
-            <div id="story-list" :style="`transform: translateX(-${this.storyCurIdx * 80}px)`">
-                <!-- <li v-for="(item, idx) in this.storyList" :key="idx">
-                    <span>{{ item.profileImg }}</span>
-                </li> -->
-                <StoryComp
-                    v-for="(item, idx) in this.storyList"
-                    :key="idx"
-                    :item-data="item"
-                    @click="showStory(idx)"
-                ></StoryComp>
-            </div>
-            <button
-                id="prev"
-                @click="slideTo(this.storyCurIdx - 5)"
-                v-show="this.storyCurIdx !== 0"
-            >
-                <span class="material-icons md-28 md-light"> arrow_circle_left </span>
-            </button>
-            <button
-                id="next"
-                @click="slideTo(this.storyCurIdx + 5)"
-                v-show="this.storyCurIdx !== 1"
-            >
-                <span class="material-icons md-28 md-light"> arrow_circle_right </span>
-            </button>
-        </section>
+    <section id="all-container">
+        <section id="feed-container">
+            <section id="story-field">
+                <div id="story-list" :style="`transform: translateX(-${this.storyCurIdx * 80}px)`">
+                    <StoryComp
+                        v-for="(item, idx) in this.storyList"
+                        :key="idx"
+                        :item-data="item"
+                        @click="showStory(idx)"
+                    ></StoryComp>
+                </div>
+                <button
+                    id="prev"
+                    @click="slideTo(this.storyCurIdx - 5)"
+                    v-show="this.storyCurIdx !== 0"
+                >
+                    <span class="material-icons md-28 md-light"> arrow_circle_left </span>
+                </button>
+                <button
+                    id="next"
+                    @click="slideTo(this.storyCurIdx + 5)"
+                    v-show="this.storyCurIdx !== 1"
+                >
+                    <span class="material-icons md-28 md-light"> arrow_circle_right </span>
+                </button>
+            </section>
 
-        <section id="feed-list">
-            <FeedComp
-                user-name="omen.mov"
-                post-location="greenwich Park"
-                :image-list="[
-                    require('@/assets/0.jpg'),
-                    require('@/assets/1.jpg'),
-                    require('@/assets/2.jpg'),
-                    require('@/assets/3.jpg'),
-                    require('@/assets/4.jpg'),
-                    require('@/assets/5.jpg'),
-                    require('@/assets/6.jpg'),
-                    require('@/assets/7.jpg'),
-                    require('@/assets/8.jpg'),
-                ]"
-            />
-            <FeedComp
-                user-name="omen.mov"
-                post-location="greenwich Park"
-                :image-list="[
-                    require('@/assets/0.jpg'),
-                    require('@/assets/1.jpg'),
-                    require('@/assets/2.jpg'),
-                    require('@/assets/3.jpg'),
-                    require('@/assets/4.jpg'),
-                    require('@/assets/5.jpg'),
-                    require('@/assets/6.jpg'),
-                    require('@/assets/7.jpg'),
-                    require('@/assets/8.jpg'),
-                ]"
-            />
-            <FeedComp
-                user-name="omen.mov"
-                post-location="greenwich Park"
-                :image-list="[
-                    require('@/assets/0.jpg'),
-                    require('@/assets/1.jpg'),
-                    require('@/assets/2.jpg'),
-                    require('@/assets/3.jpg'),
-                    require('@/assets/4.jpg'),
-                    require('@/assets/5.jpg'),
-                    require('@/assets/6.jpg'),
-                    require('@/assets/7.jpg'),
-                    require('@/assets/8.jpg'),
-                ]"
-            />
+            <section id="feed-list">
+                <FeedComp
+                    user-name="omen.mov"
+                    post-location="greenwich Park"
+                    :image-list="[
+                        require('@/assets/0.jpg'),
+                        require('@/assets/1.jpg'),
+                        require('@/assets/2.jpg'),
+                        require('@/assets/3.jpg'),
+                        require('@/assets/4.jpg'),
+                        require('@/assets/5.jpg'),
+                        require('@/assets/6.jpg'),
+                        require('@/assets/7.jpg'),
+                        require('@/assets/8.jpg'),
+                    ]"
+                />
+                <FeedComp
+                    user-name="omen.mov"
+                    post-location="greenwich Park"
+                    :image-list="[
+                        require('@/assets/0.jpg'),
+                        require('@/assets/1.jpg'),
+                        require('@/assets/2.jpg'),
+                        require('@/assets/3.jpg'),
+                        require('@/assets/4.jpg'),
+                        require('@/assets/5.jpg'),
+                        require('@/assets/6.jpg'),
+                        require('@/assets/7.jpg'),
+                        require('@/assets/8.jpg'),
+                    ]"
+                />
+                <FeedComp
+                    user-name="omen.mov"
+                    post-location="greenwich Park"
+                    :image-list="[
+                        require('@/assets/0.jpg'),
+                        require('@/assets/1.jpg'),
+                        require('@/assets/2.jpg'),
+                        require('@/assets/3.jpg'),
+                        require('@/assets/4.jpg'),
+                        require('@/assets/5.jpg'),
+                        require('@/assets/6.jpg'),
+                        require('@/assets/7.jpg'),
+                        require('@/assets/8.jpg'),
+                    ]"
+                />
+            </section>
         </section>
+        <aside id="side-container">
+            <section id="myprofile-field">
+                <img :src="require('@/assets/1.jpg')" alt="" srcset="" loading="lazy" />
+                <div class="info-field">
+                    <span class="account-id">ju_hyun.kim</span>
+                    <span class="account-name">김 주현</span>
+                </div>
+                <button id="account-change">전환</button>
+            </section>
+            <section id="recommend-field">
+                <div class="title-field">
+                    <span class="recommend-title">회원님을 위한 추천</span>
+                    <span class="recommend-all">모두 보기</span>
+                </div>
+                <div id="recommend-list">
+                    <div class="recommend-item">
+                        <img :src="require('@/assets/6.jpg')" alt="" srcset="" />
+
+                        <div class="info-field">
+                            <span class="account-id">hiking_song</span>
+                            <span class="account-name">hyeondongee님 외 8명이 팔로우합..</span>
+                        </div>
+                        <button id="follow-btn">팔로우</button>
+                    </div>
+                    <div class="recommend-item">
+                        <img :src="require('@/assets/2.jpg')" alt="" srcset="" />
+                        <div class="info-field">
+                            <span class="account-id">trueman.gogi</span>
+                            <span class="account-name">iam_ig_님 외 3명이 팔로우합니다</span>
+                        </div>
+                        <button id="follow-btn">팔로우</button>
+                    </div>
+                    <div class="recommend-item">
+                        <img :src="require('@/assets/3.jpg')" alt="" srcset="" />
+                        <div class="info-field">
+                            <span class="account-id">seokho.kang.357</span>
+                            <span class="account-name">jihye_bongbong님 외 9명이 팔로우...</span>
+                        </div>
+                        <button id="follow-btn">팔로우</button>
+                    </div>
+                    <div class="recommend-item">
+                        <img :src="require('@/assets/4.jpg')" alt="" srcset="" />
+
+                        <div class="info-field">
+                            <span class="account-id">i_so_ng</span>
+                            <span class="account-name">8urp3le님 외 6명이 팔로우합니다</span>
+                        </div>
+                        <button id="follow-btn">팔로우</button>
+                    </div>
+                    <div class="recommend-item">
+                        <img :src="require('@/assets/5.jpg')" alt="" srcset="" />
+
+                        <div class="info-field">
+                            <span class="account-id">n_m_b_j_h</span>
+                            <span class="account-name">회원님을 위한 추천</span>
+                        </div>
+                        <button id="follow-btn">팔로우</button>
+                    </div>
+                </div>
+            </section>
+            <section id="pageinfo-field"></section>
+        </aside>
     </section>
 </template>
 
